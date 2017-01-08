4d8fa112c0793f57132a743cf404b4fc0d64a9fd
Henry McC <henrymcc@gmail.com>
2017-01-08 08:21:47 +0200

    Initial Commit

---
+++ missingPersons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  missingPersons
+//
+//  Created by Henry McC on 2017/01/08.
+//  Copyright © 2017 HiddenPlatform. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
